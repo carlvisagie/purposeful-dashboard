6ab5e31e145e2fae253d4163ab027e6ec779caaf
Carl Visagie <carlhvisagie@yahoo.com>
2025-11-22 16:54:33 +0100

    Fix router imports for MVP build

---
+++ server/routes.ts
@@ -0,0 +1,15 @@
+﻿import { Hono } from "hono";
+
+// Import minimal MVP routers (default exports)
+import crisisManagement from "./routers/crisisManagement";
+import psychologicalProfiling from "./routers/psychologicalProfiling";
+import traumaCare from "./routers/traumaCare";
+
+const app = new Hono();
+
+// Attach MVP routers
+app.route("/crisis", crisisManagement);
+app.route("/psychology", psychologicalProfiling);
+app.route("/trauma", traumaCare);
+
+export default app;
